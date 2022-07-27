75956c2cf9a748e480eac5ffeff2a2c33175d5c3
bizip <bizip04@gmail.com>
2022-07-27 09:07:05 +0200

    Add decoration

--- person.rb
+++ person.rb
@@ -1,11 +1,18 @@
-class Person
+require './Nameble'
+require "./CapitalizeDecorator"
+class Person < Nameble
   attr_accessor :name, :age
   attr_reader :id
 
   def initialize(age, name = 'unknown', parent_permission: true)
+    super()
     @age = age
     @parent_permission = parent_permission
     @name = name
+  end
+
+  def correct_name
+    @name
   end
 
   def can_use_services?
--- teacher.rb
+++ teacher.rb
@@ -1,3 +1,4 @@
+require "./person"
 class Teacher < Person
   def initialize(specialization, age, _name = 'unknown', parent_permission: true)
     super(specialization, name, age, parent_permission: parent_permission)
